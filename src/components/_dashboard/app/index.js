aab8c9e496f0afe9112674c9679d0be861f50948
Saaty <71265500+Saaty@users.noreply.github.com>
2022-04-14 11:15:15 +0930

    Delete src directory

--- src/components/_dashboard/app/index.js
+++
@@ -1,8 +0,0 @@
-export { default as AppAtmosphericCO2 } from './AppAtmosphericCO2';
-export { default as AppPollution } from './AppPollution';
-export { default as AppEmissionsByRegion } from './AppEmissionsByRegion';
-export { default as AppSeaLevel } from './AppSeaLevel';
-export { default as AppSurfaceTemperature } from './AppSurfaceTemperature';
-export { default as AppClimateTimeline } from './AppClimateTimeline';
-export { default as AppGlobalCO2Emissions } from './AppGlobalCO2Emissions';
-export { default as AppDailyRainfall } from './AppDailyRainfall';
